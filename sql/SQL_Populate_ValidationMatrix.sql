28e8541b28867538eb6650865cfe06a169eca1e5
Michele Scarlato <michele.scarlato@gmail.com>
2023-03-14 11:51:33 +0100

    Removing unnecessary directories.

--- sql/SQL_Populate_ValidationMatrix.sql
+++
@@ -1,5 +0,0 @@
-TRUNCATE public.validationmatrix;
-COPY public.validationmatrix("License","Classpath exception to GPL 2.0 or later", "GPL 2.0 or later","Apache 2.0", "GPL 3.0")
-FROM '/home/michelescarlato/licenses.csv'
-DELIMITER ','
-CSV HEADER;
